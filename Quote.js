a3a2a69a3f24a8bd6894de583643549a20b40140
Rakesh Kumar <119156077+rakeshkumar04@users.noreply.github.com>
2023-04-02 21:52:30 +0530

    Add files via upload

---
+++ Quote.js
@@ -0,0 +1,9 @@
+const button = document.querySelector("button");
+const p = document.querySelector("p");
+const API = "https://api.quotable.io/random"
+
+button.addEventListener("click",() => {
+  fetch(API).then(res => res.json()).then(data => {
+    p.innerText = data.content;
+  }).catch(() => alert("Error fetching quote!"));
+});
